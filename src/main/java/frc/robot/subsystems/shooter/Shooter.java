93132b5dc75df1b90af2ee0c759153dc0546e207
NukeMinecart <145407522+NukeMinecart@users.noreply.github.com>
2026-01-19 22:07:43 -0600

    implement basic shooter

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -1,20 +1,28 @@
 package frc.robot.subsystems.shooter;
 
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
-    
-    public final ShooterIOInputsAutoLogged inputs = new ShooterIOInputsAutoLogged();
-    public final ShooterIO shooterIO;
-    public final ShooterConstants shooterConstants;
 
-    public Shooter(ShooterIO shooterIO, ShooterConstants shooterConstants) {
-        this.shooterIO = shooterIO;
-        this.shooterConstants = shooterConstants;
-    }
+  public final ShooterIOInputsAutoLogged inputs = new ShooterIOInputsAutoLogged();
+  public final ShooterIO shooterIO;
 
-    @Override
-    public void periodic(){
-        shooterIO.updateInputs(inputs);
-    }
+  public Shooter(ShooterIO shooterIO) {
+    this.shooterIO = shooterIO;
+  }
+
+  @Override
+  public void periodic() {
+    shooterIO.updateInputs(inputs);
+  }
+
+  public void setVelocity(AngularVelocity velocity) {
+    shooterIO.setVelocity(velocity);
+  }
+
+  public void setVoltage(Voltage voltage) {
+    shooterIO.setVoltage(voltage);
+  }
 }
--- src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
@@ -1,32 +1,40 @@
 package frc.robot.subsystems.shooter;
 
 public interface ShooterConstants {
-    class LeftShooterConstants implements ShooterConstants {
 
-        @Override
-        public int getTopMotorId() {
-            return 0;
-        }
+  double kP = 0;
+  double kI = 0;
+  double kD = 0;
 
-        @Override
-        public int getBottomMotorId() {
-            return 0;
-        }
+  double kV = 0;
+
+  class LeftShooterConstants implements ShooterConstants {
+
+    @Override
+    public int getTopMotorId() {
+      return 0;
     }
 
-    class RightShooterConstants implements ShooterConstants {
-
-        @Override
-        public int getTopMotorId() {
-            return 0;
-        }
-
-        @Override
-        public int getBottomMotorId() {
-            return 0;
-        }
+    @Override
+    public int getBottomMotorId() {
+      return 0;
     }
-    
-    int getTopMotorId();
-    int getBottomMotorId();
+  }
+
+  class RightShooterConstants implements ShooterConstants {
+
+    @Override
+    public int getTopMotorId() {
+      return 0;
+    }
+
+    @Override
+    public int getBottomMotorId() {
+      return 0;
+    }
+  }
+
+  int getTopMotorId();
+
+  int getBottomMotorId();
 }
