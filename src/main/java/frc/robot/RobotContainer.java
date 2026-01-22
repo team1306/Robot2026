26d8e96b8675a1a31f72c0f1ad2fca47ee08aa5f
Connor Mulligan <cgmulligan33@gmail.com>
2026-01-21 20:18:11 -0600

    fixed robotcontainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,3 +1,4 @@
+
 // Copyright (c) 2021-2026 Littleton Robotics
 // http://github.com/Mechanical-Advantage
 //
@@ -7,6 +8,10 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.controls.Controls;
 import frc.robot.generated.TunerConstants;
@@ -16,6 +21,8 @@ import frc.robot.subsystems.drive.GyroIOPigeon2;
 import frc.robot.subsystems.drive.ModuleIO;
 import frc.robot.subsystems.drive.ModuleIOSim;
 import frc.robot.subsystems.drive.ModuleIOTalonFX;
+import frc.robot.subsystems.vision.Vision;
+import frc.robot.subsystems.vision.VisionIOPhotonVision;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -26,6 +33,7 @@ import frc.robot.subsystems.drive.ModuleIOTalonFX;
 public class RobotContainer {
   // Subsystems
   private final Drive drive;
+  private final Vision vision;
 
   private final Controls controls;
   private final Autos autos;
@@ -68,6 +76,44 @@ public class RobotContainer {
 
     controls = new Controls(drive);
     autos = new Autos(drive);
+
+    vision = initializeVision();
+  }
+
+  private Vision initializeVision() {
+    drive.setVisionStdDevs(VecBuilder.fill(1, 1, 1));
+
+    return new Vision(
+        drive::addVisionMeasurement,
+        new VisionIOPhotonVision(
+            "frontLeft",
+            new Transform3d(
+                0.307325,
+                0.307325,
+                0.215781,
+                new Rotation3d(0, Units.degreesToRadians(-45), Units.degreesToRadians(45)))),
+        new VisionIOPhotonVision(
+            "frontRight",
+            new Transform3d(
+                0.307325,
+                -0.307325,
+                0.215781,
+                new Rotation3d(0, Units.degreesToRadians(-45), Units.degreesToRadians(-45)))),
+        new VisionIOPhotonVision(
+            "backLeft",
+            new Transform3d(
+                -0.307325,
+                0.307325,
+                0.215781,
+                new Rotation3d(0, Units.degreesToRadians(-45), Units.degreesToRadians(135)))),
+        new VisionIOPhotonVision(
+            "backRight",
+            new Transform3d(
+                -0.307325,
+                -0.307325,
+                0.215781,
+                new Rotation3d(0, Units.degreesToRadians(-45), Units.degreesToRadians(-135))))
+    );
   }
 
   public Command getAutonomousCommand() {
