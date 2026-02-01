860941a8fa1342c1b8b567e0ec673e8b307c5b46
Eli Bailey <eli.bailey.376@gmail.com>
2026-01-31 21:17:08 -0600

    Add final to constants

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -1,7 +1,7 @@
 package frc.robot.subsystems.intake;
 
 public class IntakeConstants {
-  public static int INTAKE_LEFT_MOTOR_ID = 0;
-  public static int INTAKE_RIGHT_MOTOR_ID = 0;
-  public static int LATCH_MOTOR_ID = 0;
+  public static final int INTAKE_LEFT_MOTOR_ID = 0;
+  public static final int INTAKE_RIGHT_MOTOR_ID = 0;
+  public static final int LATCH_MOTOR_ID = 0;
 }
