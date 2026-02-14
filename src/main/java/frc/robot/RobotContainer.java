4d276dea7432154bbb3f6a80fb97d20a7eeb66b6
Connor Mulligan <cgmulligan33@gmail.com>
2026-02-14 12:00:31 -0600

    fixed robot container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,12 +11,18 @@ import frc.robot.subsystems.drive.ModuleIOSim;
 import frc.robot.subsystems.drive.ModuleIOTalonFX;
 import frc.robot.subsystems.indexer.Indexer;
 import frc.robot.subsystems.indexer.IndexerIO;
+import frc.robot.subsystems.indexer.IndexerIOReal;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.intake.IntakeIO;
+import frc.robot.subsystems.intake.IntakeIOReal;
 import frc.robot.subsystems.shooter.Shooter;
 import frc.robot.subsystems.shooter.ShooterIO;
+import frc.robot.subsystems.shooter.ShooterIOReal;
 import frc.robot.subsystems.vision.Vision;
+import frc.robot.subsystems.vision.VisionConstants;
 import frc.robot.subsystems.vision.VisionIO;
+import frc.robot.subsystems.vision.VisionIOPhotonVision;
+import frc.robot.subsystems.vision.VisionIOPhotonVisionSim;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -46,16 +52,22 @@ public class RobotContainer {
                 new ModuleIOTalonFX(TunerConstants.FrontRight),
                 new ModuleIOTalonFX(TunerConstants.BackLeft),
                 new ModuleIOTalonFX(TunerConstants.BackRight));
-        intake = new Intake(new IntakeIO() {});
-        indexer = new Indexer(new IndexerIO() {});
-        shooter = new Shooter(new ShooterIO() {});
+        intake = new Intake(new IntakeIOReal());
+        indexer = new Indexer(new IndexerIOReal());
+        shooter = new Shooter(new ShooterIOReal());
         vision =
             new Vision(
                 drive::addVisionMeasurement,
-                new VisionIO() {},
-                new VisionIO() {},
-                new VisionIO() {},
-                new VisionIO() {});
+                new VisionIOPhotonVision(
+                    VisionConstants.LEFT_CAMERA_NAME, VisionConstants.LEFT_CAMERA_POSITION),
+                new VisionIOPhotonVision(
+                    VisionConstants.BACK_LEFT_CAMERA_NAME,
+                    VisionConstants.BACK_LEFT_CAMERA_POSITION),
+                new VisionIOPhotonVision(
+                    VisionConstants.BACK_RIGHT_CAMERA_NAME,
+                    VisionConstants.BACK_RIGHT_CAMERA_POSITION),
+                new VisionIOPhotonVision(
+                    VisionConstants.RIGHT_CAMERA_NAME, VisionConstants.RIGHT_CAMERA_POSITION));
         break;
 
       case SIM:
@@ -67,16 +79,28 @@ public class RobotContainer {
                 new ModuleIOSim(TunerConstants.FrontRight),
                 new ModuleIOSim(TunerConstants.BackLeft),
                 new ModuleIOSim(TunerConstants.BackRight));
-        intake = new Intake(new IntakeIO() {});
-        indexer = new Indexer(new IndexerIO() {});
-        shooter = new Shooter(new ShooterIO() {});
+        intake = new Intake(new IntakeIOReal());
+        indexer = new Indexer(new IndexerIOReal());
+        shooter = new Shooter(new ShooterIOReal());
         vision =
             new Vision(
                 drive::addVisionMeasurement,
-                new VisionIO() {},
-                new VisionIO() {},
-                new VisionIO() {},
-                new VisionIO() {});
+                new VisionIOPhotonVisionSim(
+                    VisionConstants.LEFT_CAMERA_NAME,
+                    VisionConstants.LEFT_CAMERA_POSITION,
+                    () -> drive.getPose()),
+                new VisionIOPhotonVisionSim(
+                    VisionConstants.BACK_LEFT_CAMERA_NAME,
+                    VisionConstants.BACK_LEFT_CAMERA_POSITION,
+                    () -> drive.getPose()),
+                new VisionIOPhotonVisionSim(
+                    VisionConstants.BACK_RIGHT_CAMERA_NAME,
+                    VisionConstants.BACK_RIGHT_CAMERA_POSITION,
+                    () -> drive.getPose()),
+                new VisionIOPhotonVisionSim(
+                    VisionConstants.RIGHT_CAMERA_NAME,
+                    VisionConstants.RIGHT_CAMERA_POSITION,
+                    () -> drive.getPose()));
         break;
 
       default:
