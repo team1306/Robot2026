fc001d1151ce0472a830c9fcd9e8d365dd54f047
NukeMinecart <145407522+NukeMinecart@users.noreply.github.com>
2026-02-16 18:17:55 -0600

    make safe shoot to the corners work

--- src/main/java/frc/robot/commands/SafeShootCommand.java
+++ src/main/java/frc/robot/commands/SafeShootCommand.java
@@ -4,14 +4,15 @@ import static edu.wpi.first.units.Units.Meters;
 
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.indexer.Indexer;
 import frc.robot.subsystems.shooter.Shooter;
-import frc.robot.util.RebuiltUtils;
 import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
 import java.util.function.Supplier;
+import org.littletonrobotics.junction.Logger;
 
 public class SafeShootCommand extends ParallelCommandGroup {
 
@@ -28,11 +29,7 @@ public class SafeShootCommand extends ParallelCommandGroup {
             shooter,
             () -> Meters.of(drive.getPose().getTranslation().getDistance(positionSupplier.get())));
     Command driveAtAngleCommand =
-        DriveCommands.driveAimLockedCommand(
-            drive,
-            xSupplier,
-            ySupplier,
-            () -> RebuiltUtils.getCurrentHubLocation().toTranslation2d());
+        DriveCommands.driveAimLockedCommand(drive, xSupplier, ySupplier, positionSupplier);
     Command indexerCommand = indexer.indexUntilCancelledCommand(1);
 
     BooleanSupplier shooterVelocityCondition = shooter.isAtRequestedSpeed();
@@ -42,6 +39,14 @@ public class SafeShootCommand extends ParallelCommandGroup {
     Command guardedIndexerCommand =
         new GuardedCommand(indexerCommand, shooterVelocityCondition, driveAngleCondition);
 
-    addCommands(shootAtDistanceCommand, driveAtAngleCommand, guardedIndexerCommand);
+    Command loggedGuardCommand =
+        Commands.run(
+            () ->
+                Logger.recordOutput(
+                    "Controls/Ready To Shoot",
+                    shooterVelocityCondition.getAsBoolean() && driveAngleCondition.getAsBoolean()));
+
+    addCommands(
+        shootAtDistanceCommand, driveAtAngleCommand, guardedIndexerCommand, loggedGuardCommand);
   }
 }
