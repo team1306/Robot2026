bff4365858b6ef6b9f03a29bb8b8d4af3101ca36
Connor Mulligan <cgmulligan33@gmail.com>
2026-02-18 15:44:20 -0600

    added subscriber for target speed

--- src/main/java/frc/robot/controls/CompetitionControllerMapping.java
+++ src/main/java/frc/robot/controls/CompetitionControllerMapping.java
@@ -4,18 +4,25 @@ import static edu.wpi.first.units.Units.Meters;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
 import static edu.wpi.first.units.Units.Seconds;
 
+import org.littletonrobotics.junction.AutoLogOutput;
+
 import badgerutils.commands.CommandUtils;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.commands.ShooterCommands;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.shooter.Shooter;
+import frc.robot.util.LoggedNetworkNumberPlus;
 
 public class CompetitionControllerMapping extends ControllerMapping {
 
   private final Drive drive;
   private final Intake intake;
   private final Shooter shooter;
+
+  @AutoLogOutput
+  private final LoggedNetworkNumberPlus targetSpeed =
+      new LoggedNetworkNumberPlus("/Tuning/Shooter RPS", 0);
 
   public CompetitionControllerMapping(
       CommandXboxController driverController,
@@ -31,29 +38,7 @@ public class CompetitionControllerMapping extends ControllerMapping {
 
   @Override
   public void bind() {
-    // A button: while held, shooter rpm goes from 10-20 rps based on left trigger
-    driverController
-        .a()
-        .whileTrue(
-            ShooterCommands.shootAtDistanceCommand(
-                shooter, () -> Meters.of(driverController.getLeftTriggerAxis() + 1))); // 1-2 meters
-
-    // B button: while held shooter rpm goes to 20 rps for 2 seconds
-    driverController
-        .b()
-        .onTrue(ShooterCommands.shootForTimeCommand(shooter, () -> Meters.of(2), Seconds.of(2)));
-
-    // X button: while held, shooter rps goes to 10 rps;
-    driverController
-        .x()
-        .whileTrue(ShooterCommands.shootAtSpeedCommand(shooter, RotationsPerSecond.of(10)));
-
-    // Y button: while held, shooter rps goes from 0 to 10 rps based on left trigger
-    driverController
-        .y()
-        .whileTrue(
-            ShooterCommands.shootAtSpeedCommand(
-                shooter, () -> RotationsPerSecond.of(10 * driverController.getLeftTriggerAxis())));
+    driverController.a().whileTrue(ShooterCommands.shootAtSpeedCommand(shooter, () -> RotationsPerSecond.of(targetSpeed.get())));
   }
 
   @Override
