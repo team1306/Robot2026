550e48f5f12ff45748a9a2b5f0337c03c959d1c0
Connor Mulligan <cgmulligan33@gmail.com>
2026-02-21 10:48:03 -0600

    Commented and lowered speed on reversing

--- src/main/java/frc/robot/controls/CompetitionControllerMapping.java
+++ src/main/java/frc/robot/controls/CompetitionControllerMapping.java
@@ -58,6 +58,8 @@ public class CompetitionControllerMapping extends ControllerMapping {
                     RebuiltUtils.isInAllianceZone(drive.getPose().getTranslation())
                         ? RebuiltUtils.getCurrentHubLocation().toTranslation2d()
                         : RebuiltUtils.getNearestAllianceCorner(drive.getPose().getTranslation())));
+    
+    // Default Drive Control
     drive.setDefaultCommand(
         DriveCommands.joystickDriveCommand(
             drive,
@@ -66,6 +68,7 @@ public class CompetitionControllerMapping extends ControllerMapping {
             () -> -driverController.getRightX()));
     driverController.a().whileTrue(indexer.indexUntilCancelledCommand(0.5));
 
+    // Reset Odometry
     driverController
         .start()
         .onTrue(
@@ -76,10 +79,13 @@ public class CompetitionControllerMapping extends ControllerMapping {
                     drive)
                 .ignoringDisable(true));
 
+    // Intake
     driverController.leftTrigger(0.5).whileTrue(intake.intakeUntilInterruptedCommand(1));
 
+    // Fuel Collection
     driverController.b().whileTrue(new FuelCollectionCommand(drive, fuelDetection));
 
+    // Robot Relative Drive
     driverController
         .x()
         .whileTrue(
@@ -89,12 +95,14 @@ public class CompetitionControllerMapping extends ControllerMapping {
                 () -> -driverController.getLeftX(),
                 () -> -driverController.getRightX()));
 
+    // Snake Mode
     driverController
         .y()
         .whileTrue(
             new FaceforwardCommand(
                 drive, () -> -driverController.getLeftY(), () -> -driverController.getLeftX()));
 
+    // Shoot to Hub
     driverController
         .rightBumper()
         .whileTrue(
@@ -106,6 +114,7 @@ public class CompetitionControllerMapping extends ControllerMapping {
                 () -> -driverController.getLeftX(),
                 () -> RebuiltUtils.getCurrentHubLocation().toTranslation2d()));
 
+    // Shoot to Corner
     driverController
         .leftBumper()
         .whileTrue(
@@ -117,6 +126,7 @@ public class CompetitionControllerMapping extends ControllerMapping {
                 () -> -driverController.getLeftX(),
                 () -> RebuiltUtils.getNearestAllianceCorner(drive.getPose().getTranslation())));
 
+    // Shoot to Hub or Corner Depending on Location
     driverController
         .rightTrigger()
         .whileTrue(
@@ -132,8 +142,10 @@ public class CompetitionControllerMapping extends ControllerMapping {
                             : RebuiltUtils.getNearestAllianceCorner(
                                 drive.getPose().getTranslation()))
                 .alongWith(loggedTargetCommand));
-    // P2 -- ME!!!
 
+    /* ---P2--- */
+
+    // Jumble Indexer
     operatorController
         .leftTrigger(0.1)
         .whileTrue(
@@ -145,6 +157,8 @@ public class CompetitionControllerMapping extends ControllerMapping {
                             operatorController.setRumble(
                                 RumbleType.kBothRumble, operatorController.getLeftTriggerAxis()))))
         .onFalse(new InstantCommand(() -> operatorController.setRumble(RumbleType.kBothRumble, 0)));
+    
+    // Spool Shooter
     operatorController
         .rightTrigger()
         .whileTrue(
@@ -164,37 +178,42 @@ public class CompetitionControllerMapping extends ControllerMapping {
 
     // Overides
 
+    // Brake Shooter
     operatorController
         .leftBumper()
         .onTrue(new InstantCommand(() -> shooter.setVelocity(RotationsPerSecond.of(0)))); // brake
 
+    // Override Shooter by +0.5 RPS
     operatorController
         .povUp()
         .onTrue(
             new InstantCommand(() -> shooter.changeVelocityOverride(RotationsPerSecond.of(0.5))));
+    
+    // Override Shooter by -0.5 RPS
     operatorController
         .povDown()
         .onTrue(
             new InstantCommand(() -> shooter.changeVelocityOverride(RotationsPerSecond.of(-0.5))));
 
+    // Reset overrides
     operatorController.start().onTrue(new InstantCommand(() -> shooter.resetVelocityOverride()));
 
-    // operatorController
-    //     .povLeft()
-    //     .onTrue(new InstantCommand(() -> intake.));
-    // Commented out untill spool command
-
+    // Reverse Shooter
     operatorController
         .y()
         .onTrue(ShooterCommands.shootAtSpeedCommand(shooter, () -> RotationsPerSecond.of(-20)))
         .onFalse(new InstantCommand(() -> shooter.setIdle()));
+    
+    // Reverse Indexer
     operatorController
         .b()
-        .onTrue(new InstantCommand(() -> indexer.setDutyCycle(-1)))
+        .onTrue(new InstantCommand(() -> indexer.setDutyCycle(-0.25)))
         .onFalse(new InstantCommand(() -> indexer.setDutyCycle(0)));
+
+    // Reverse Intake
     operatorController
         .a()
-        .onTrue(new InstantCommand(() -> intake.setDutyCycle(-1)))
+        .onTrue(new InstantCommand(() -> intake.setDutyCycle(-0.5)))
         .onFalse(new InstantCommand(() -> intake.setDutyCycle(0)));
   }
 
