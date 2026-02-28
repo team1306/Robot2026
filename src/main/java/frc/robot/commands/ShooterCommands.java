bcb5ba7beb9f983d5da6cee076ad39ce6507b420
Connor Mulligan <cgmulligan33@gmail.com>
2026-02-28 15:24:42 -0600

    fixed numbers in shootercommands

--- src/main/java/frc/robot/commands/ShooterCommands.java
+++ src/main/java/frc/robot/commands/ShooterCommands.java
@@ -23,15 +23,9 @@ public class ShooterCommands {
   public static final ShooterSetpoint[] SETPOINTS =
       Arrays.stream(
               new ShooterSetpoint[] {
-                new ShooterSetpoint(Meters.of(0), RotationsPerSecond.of(0), Seconds.of(0)),
-                new ShooterSetpoint(Meters.of(1), RotationsPerSecond.of(10), Seconds.of(0.15)),
-                new ShooterSetpoint(Meters.of(2), RotationsPerSecond.of(20), Seconds.of(0.30)),
-                new ShooterSetpoint(Meters.of(3), RotationsPerSecond.of(30), Seconds.of(0.45)),
-                new ShooterSetpoint(Meters.of(4), RotationsPerSecond.of(40), Seconds.of(0.70)),
-                new ShooterSetpoint(Meters.of(5), RotationsPerSecond.of(50), Seconds.of(0.85)),
                 new ShooterSetpoint(Feet.of(7.5), RotationsPerSecond.of(41), Seconds.of(0.7875)),
                 new ShooterSetpoint(Feet.of(9), RotationsPerSecond.of(42), Seconds.of(0.8209)),
-                new ShooterSetpoint(Feet.of(12), RotationsPerSecond.of(48), Seconds.of(1.0416)),
+                new ShooterSetpoint(Feet.of(12), RotationsPerSecond.of(48), Seconds.of(1.0146)),
                 new ShooterSetpoint(Feet.of(15), RotationsPerSecond.of(53.5), Seconds.of(1.2209)),
                 new ShooterSetpoint(Feet.of(18), RotationsPerSecond.of(59.75), Seconds.of(1.3584)),
               })
