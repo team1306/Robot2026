d99282904241d92c54d67ce66f3d03e0a82a5df7
ElectricTurtle32 <49703859+ElectricTurtle32@users.noreply.github.com>
2026-03-28 12:45:53 -0500

    add state machine

--- src/main/java/frc/robot/subsystems/leds/Leds.java
+++ src/main/java/frc/robot/subsystems/leds/Leds.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems.leds;
 
+import badgerutils.statemachine.Edges;
+import badgerutils.statemachine.StateMachine;
 import badgerutils.triggers.AllianceTriggers;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -10,6 +12,15 @@ public class Leds extends SubsystemBase {
   public final LedsIO LedsIO;
   public boolean isShooting = false;
   public boolean isInShootingTolerance = false;
+  private StateMachine<LedState> stateMachine;
+
+  public enum LedState {
+    DISABLED,
+    AUTO,
+    AIMING,
+    SHOOTING,
+    OTHER
+  }
 
   public Leds(LedsIO LedsIO) {
     this.LedsIO = LedsIO;
@@ -17,22 +28,31 @@ public class Leds extends SubsystemBase {
 
   @Override
   public void periodic() {
+    Edges<LedState> edges =
+        new Edges<LedState>()
+            .anyToState(LedState.DISABLED, state -> LedsIO.setRainbow(4))
+            .anyToState(LedState.AUTO, state -> LedsIO.setBlink(255, 0, 255, 4))
+            .anyToState(LedState.AIMING, state -> LedsIO.setSolid(0, 150, 0))
+            .anyToState(LedState.SHOOTING, state -> LedsIO.setBlink(0, 255, 0, 4))
+            .anyToState(
+                LedState.OTHER,
+                state ->
+                    LedsIO.setSolid(
+                        AllianceTriggers.isRedAlliance() ? 150 : 0,
+                        0,
+                        AllianceTriggers.isRedAlliance() ? 0 : 150));
+
+    stateMachine = new StateMachine<>(LedState.DISABLED, edges);
     if (DriverStation.isDisabled()) {
-
-      LedsIO.setRainbow(4);
-
+      stateMachine.tryChangeState(LedState.DISABLED);
     } else if (DriverStation.isAutonomous()) {
-      LedsIO.setSolid(150, 0, 150);
+      stateMachine.tryChangeState(LedState.AUTO);
     } else if (isShooting && !isInShootingTolerance) {
-
-      LedsIO.setSolid(0, 150, 0);
+      stateMachine.tryChangeState(LedState.AIMING);
     } else if (isShooting) {
-      LedsIO.setBlink(0, 150, 0, 4);
+      stateMachine.tryChangeState(LedState.SHOOTING);
     } else {
-      LedsIO.setSolid(
-          AllianceTriggers.isRedAlliance() ? 150 : 0,
-          0,
-          AllianceTriggers.isRedAlliance() ? 0 : 150);
+      stateMachine.tryChangeState(LedState.OTHER);
     }
 
     Logger.recordOutput("Leds/isShooting", isShooting);
