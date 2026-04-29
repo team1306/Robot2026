2ba52032c98c3f2e5de6dfce4bbfc1a28a28b656
Connor Mulligan <cgmulligan33@gmail.com>
2026-04-29 12:27:41 -0500

    migrated spooling change

--- src/main/java/frc/robot/controls/CompetitionControllerMapping.java
+++ src/main/java/frc/robot/controls/CompetitionControllerMapping.java
@@ -226,8 +226,11 @@ public class CompetitionControllerMapping extends ControllerMapping {
                     () -> ShooterCommands.HUB_SETPOINTS)
                 .withInterruptBehavior(InterruptionBehavior.kCancelSelf)
                 .alongWith(
-                    new InstantCommand(
-                        () -> operatorController.setRumble(RumbleType.kBothRumble, 0.25))))
+                    new InstantCommand(() -> operatorController.setRumble(RumbleType.kBothRumble, 0.25)))
+                .alongWith(
+                    (booster.boostCommand(-0.5).alongWith(indexer.indexUntilCancelledCommand(-0.5)))
+                        .withDeadline(Commands.waitSeconds(0.25))
+                        .andThen(booster.boostCommand(.8))))
         .onFalse(
             new InstantCommand(() -> operatorController.setRumble(RumbleType.kBothRumble, 0))
                 .ignoringDisable(true));
